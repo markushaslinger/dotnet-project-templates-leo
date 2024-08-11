9fbf7314fdabc35efd457ac9c2d44f1e044a84e0
Markus <markus@haslinger.it>
2024-08-11 16:05:59 +0200

    added minimal api template

---
+++ templates/leominiapi/Core/Setup.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using NodaTime.Serialization.SystemTextJson;
+
+namespace LeoMiniApi.Core;
+
+public static class Setup
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IClock>(SystemClock.Instance);
+    }
+
+    public static void ConfigureServices(this IServiceCollection services, bool isDevelopment)
+    {
+        services.ConfigureHttpJsonOptions(options =>
+        {
+            options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.SerializerOptions.WriteIndented = isDevelopment;
+            options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+            options.SerializerOptions.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+        });
+    }
+}
---
+++ templates/leominiapi/Program.cs
@@ -0,0 +1,39 @@
+using LeoMiniApi.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.RegisterServices();
+builder.Services.ConfigureServices(builder.Environment.IsDevelopment());
+
+var app = builder.Build();
+
+if(builder.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+}
+
+app.MapGet("/hello/{name}", (string? name, IClock clock) =>
+   {
+       if (string.IsNullOrWhiteSpace(name))
+       {
+           return Results.BadRequest();
+       }
+
+       var now = clock.GetCurrentInstant();
+
+       return Results.Ok(new Greeting(name, now));
+   })
+   .Produces<Greeting>(StatusCodes.Status200OK)
+   .Produces(StatusCodes.Status400BadRequest)
+   .WithName("Greetings")
+   .WithOpenApi();
+
+await app.RunAsync();
+
+internal sealed class Greeting(string name, Instant timestamp)
+{
+    public string Message => $"Hello, {name}!";
+    public Instant Timestamp => timestamp;
+}
