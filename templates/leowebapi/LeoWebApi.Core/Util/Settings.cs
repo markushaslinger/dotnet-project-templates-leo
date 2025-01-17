9dbf56316e36618d197e76b7173f51509ee609da
Markus Haslinger <markus@haslinger.it>
2025-01-17 18:49:44 +0100

    immutable settings file

--- templates/leowebapi/LeoWebApi.Core/Util/Settings.cs
+++ templates/leowebapi/LeoWebApi.Core/Util/Settings.cs
@@ -3,7 +3,7 @@ namespace LeoWebApi.Core.Util;
 public sealed class Settings
 {
     public const string SectionKey = "General";
-    public double MaxPayloadWeight { get; set; }
-    public List<string> ValidLaunchSites { get; set; } = [];
-    public required string ClientOrigin { get; set; }
+    public double MaxPayloadWeight { get; init; }
+    public IReadOnlyList<string> ValidLaunchSites { get; init; } = [];
+    public required string ClientOrigin { get; init; }
 }
